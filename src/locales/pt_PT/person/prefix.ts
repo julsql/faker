ab927a6b895cdddf387284920264a88163354911
ST-DDT <ST-DDT@gmx.de>
2024-12-03 14:10:24 +0100

    chore: restore pt person prefix comments

--- src/locales/pt_PT/person/prefix.ts
+++ src/locales/pt_PT/person/prefix.ts
@@ -1,13 +1,22 @@
 export default {
   female: [
-    'Adv.ª',
-    'Arq.ª',
-    'Dra.',
-    'Enf.ª',
-    'Eng.ª',
-    'Prof.ª',
-    'Sra.',
-    'Téc.ª',
+    'Adv.ª', // Advogada (Lawyer, female)
+    'Arq.ª', // Arquiteta (Architect, female)
+    'Dra.', // Doutora (Doctor, female)
+    'Enf.ª', // Enfermeira (Nurse, female)
+    'Eng.ª', // Engenheira (Engineer, female)
+    'Prof.ª', // Professora (female)
+    'Sra.', // Senhora (Mrs.)
+    'Téc.ª', // Técnica (Technician, female)
   ],
-  male: ['Adv.', 'Arq.', 'Dr.', 'Enf.', 'Eng.º', 'Prof.', 'Sr.', 'Téc.'],
+  male: [
+    'Adv.', // Advogado (Lawyer, male)
+    'Arq.', // Arquiteto (Architect, male)
+    'Dr.', // Doutor (Doctor, male)
+    'Enf.', // Enfermeiro (Nurse, male)
+    'Eng.º', // Engenheiro (Engineer, male)
+    'Prof.', // Professor (male)
+    'Sr.', // Senhor (Mr.)
+    'Téc.', // Técnico (Technician, male)
+  ],
 };
